1b21bef2ba4e39e91f8b7e366830974aba7aa8ac
LucasWongC <wm111898@gmail.com>
2025-03-13 21:44:07 -0400

    added deploy script

--- scripts/DeployBridges.s.sol
+++ scripts/DeployBridges.s.sol
@@ -6,11 +6,12 @@ import {IERC20} from 'openzeppelin-contracts/contracts/token/ERC20/IERC20.sol';
 import {GovernanceV3Ethereum} from 'aave-address-book/GovernanceV3Ethereum.sol';
 import {GovernanceV3Optimism} from 'aave-address-book/GovernanceV3Optimism.sol';
 import {GovernanceV3Polygon} from 'aave-address-book/GovernanceV3Polygon.sol';
-import {ArbitrumScript, EthereumScript, OptimismScript, PolygonScript} from 'solidity-utils/contracts/utils/ScriptUtils.sol';
+import {ArbitrumScript, EthereumScript, OptimismScript, PolygonScript, SonicScript} from 'solidity-utils/contracts/utils/ScriptUtils.sol';
 import {AaveArbEthERC20Bridge} from 'src/bridges/arbitrum/AaveArbEthERC20Bridge.sol';
 import {AavePolEthERC20Bridge} from 'src/bridges/polygon/AavePolEthERC20Bridge.sol';
 import {AavePolEthPlasmaBridge} from 'src/bridges/polygon/AavePolEthPlasmaBridge.sol';
 import {AaveOpEthERC20Bridge} from 'src/bridges/optimism/AaveOpEthERC20Bridge.sol';
+import {AaveSonicEthERC20Bridge} from 'src/bridges/sonic/AaveSonicEthERC20Bridge.sol';
 
 contract DeployEthereum is EthereumScript {
   function run() external broadcast {
@@ -58,5 +59,25 @@ contract DeployArbBridgeArbitrum is ArbitrumScript {
   function run() external broadcast {
     bytes32 salt = 'Aave Treasury Bridge';
     new AaveArbEthERC20Bridge{salt: salt}(0x3765A685a401622C060E5D700D9ad89413363a91);
+  }
+}
+
+contract DeploySonicBridgeEthereum is EthereumScript {
+  function run() external broadcast {
+    bytes32 salt = 'Aave Sonic ERC20 Bridge';
+    new AaveSonicEthERC20Bridge{salt: salt}(
+      0x94A8518B76A3c45F5387B521695024379d43d715,
+      0x94A8518B76A3c45F5387B521695024379d43d715
+    );
+  }
+}
+
+contract DeploySonicBridgeSonic is SonicScript {
+  function run() external broadcast {
+    bytes32 salt = 'Aave Sonic ERC20 Bridge';
+    new AaveSonicEthERC20Bridge{salt: salt}(
+      0x94A8518B76A3c45F5387B521695024379d43d715,
+      0x94A8518B76A3c45F5387B521695024379d43d715
+    );
   }
 }
