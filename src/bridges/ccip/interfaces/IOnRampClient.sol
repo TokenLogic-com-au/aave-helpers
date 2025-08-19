d85faf5f0f7f1aa193d5b27fad866aba1b1a54ac
efecarranza <fcarr004@fiu.edu>
2025-08-19 19:16:12 -0300

    feat: allow out of order execution

--- src/bridges/ccip/interfaces/IOnRampClient.sol
+++ src/bridges/ccip/interfaces/IOnRampClient.sol
@@ -2,6 +2,9 @@
 pragma solidity ^0.8.0;
 
 interface IOnRampClient {
-    /// @notice Get the pool for a specific token
-    function getPoolBySourceToken(uint64 destChainSelector, address sourceToken) external view returns (address);
+  /// @notice Get the pool for a specific token
+  function getPoolBySourceToken(
+    uint64 destChainSelector,
+    address sourceToken
+  ) external view returns (address);
 }
--- tests/bridges/ccip/IGhoToken.sol
+++ tests/bridges/ccip/IGhoToken.sol
@@ -6,19 +6,19 @@ pragma solidity ^0.8.0;
  * @author Aave
  */
 interface IGhoToken {
-    /**
-     * @notice Set the bucket capacity of the facilitator.
-     * @dev Only accounts with `BUCKET_MANAGER_ROLE` role can call this function
-     * @param facilitator The address of the facilitator
-     * @param newCapacity The new capacity of the bucket
-     */
-    function setFacilitatorBucketCapacity(address facilitator, uint128 newCapacity) external;
+  /**
+   * @notice Set the bucket capacity of the facilitator.
+   * @dev Only accounts with `BUCKET_MANAGER_ROLE` role can call this function
+   * @param facilitator The address of the facilitator
+   * @param newCapacity The new capacity of the bucket
+   */
+  function setFacilitatorBucketCapacity(address facilitator, uint128 newCapacity) external;
 
-    /**
-     * @notice Returns the bucket configuration of the facilitator
-     * @param facilitator The address of the facilitator
-     * @return The capacity of the facilitator's bucket
-     * @return The level of the facilitator's bucket
-     */
-    function getFacilitatorBucket(address facilitator) external view returns (uint256, uint256);
+  /**
+   * @notice Returns the bucket configuration of the facilitator
+   * @param facilitator The address of the facilitator
+   * @return The capacity of the facilitator's bucket
+   * @return The level of the facilitator's bucket
+   */
+  function getFacilitatorBucket(address facilitator) external view returns (uint256, uint256);
 }
